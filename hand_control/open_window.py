05e6f9ad43b23037dfbe6e32071f3d067185c80c
jojo <jojo@linuxhomebox.myguest.virtualbox.org>
2023-04-05 21:02:18 -0700

    second instance

--- hand_control/open_window.py
+++ hand_control/open_window.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, mediapipe as mp
 
 cam = cv2.VideoCapture(0)
 print(cam.isOpened())
--- initiation.sh
+++ initiation.sh
@@ -1,6 +1,13 @@
 sudo apt update;
-sudo apt install tmux tmuxinator ranger docker.io docker -y --force-yes;
+sudo apt install tmux tmuxinator ranger docker.io docker curl python3-pip -y;
 sudo snap install bpytop;
 
 sudo addgroup docker; sudo usermod -aG docker $USER;
 newgrp docker;
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker};
+mkdir -p $DOCKER_CONFIG/cli-plugins;
+curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose;
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose;
+
+#pip install "https://github.com/SimplyPrint/OctoPrint-Creality2xTemperatureReportingFix/archive/master.zip"
