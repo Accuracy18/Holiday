8e69d58b61c722cd313134c5241789f420e07b05
Accuracy18 <tadesco18@gmail.com>
2023-04-07 06:36:39 -0400

    third instance

--- hand_control/open_window.py
+++ hand_control/open_window.py
@@ -1,4 +1,20 @@
-import cv2, mediapipe as mp
+import cv2
 
-cam = cv2.VideoCapture(0)
-print(cam.isOpened())
+
+class FrameStuff(object):
+    cam = cv2.VideoCapture('/dev/video0')
+    print(cam.isOpened())
+    
+    cv2.namedWindow("Main Frame")
+    
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+        
+    def update(self):
+        while self.cam.isOpened():
+            _ret, frame = self.cam.read()
+            print(frame)
+            
+            cv2.imshow("Main Frame", frame)
+        
+FrameStuff().update()
