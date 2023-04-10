b4b7408a6229e18065a93f718a9d4dd8b66d0307
jojo <jojo@linuxhomebox.myguest.virtualbox.org>
2023-04-10 01:01:52 -0500

    forth instance

--- hand_control/open_window.py
+++ hand_control/open_window.py
@@ -1,18 +1,18 @@
 import cv2
 
 class FrameStuff(object):
-    cam = cv2.VideoCapture('/dev/video0')
+    cam = cv2.VideoCapture(0)
     print(cam.isOpened())
-    
-    cv2.namedWindow("Main Frame")
     
     def __init__(self, **kwargs):
         self.__dict__.update(**kwargs)
         
     def update(self):
         while self.cam.isOpened():
-            _ret, frame = self.cam.read()
-            print(frame)            
-            cv2.imshow("Main Frame", frame)
+            _, frame = self.cam.read()
+            cv2.imshow("Main", frame)
+
+            if cv2.waitKey(1) == ord('q'): break
+            print(frame)
         
 FrameStuff().update()
